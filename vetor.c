3c0441015ea6f03b59ef09538dffbfee5f73534f
Julyane Ribeiro <rjulyane633@gmail.com>
2026-05-05 08:08:39 -0300

    Update vetor.c

--- vetor.c
+++ vetor.c
@@ -15,6 +15,14 @@ int main(){
 	for(j=i-1;j>=0;j--){
 		printf(" %c", vetorzin[j]);
 	}    
-    
+    printf("vogais presentes: ");
+	for(j=0;j<i;j++){
+		if (vetorzin[j]=='a' || vetorzin[j]=='e' || vetorzin[j]=='i' || vetorzin[j]=='o' || vetorzin[j]=='u'){
+			printf(" %c", vetorzin[j]);
+		}
+		else if (vetorzin[j]=='A' || vetorzin[j]=='E' || vetorzin[j]=='I' || vetorzin[j]=='O' || vetorzin[j]=='U'){
+			printf(" %c", vetorzin[j]);
+		}
+	} 
     return 0;
 }
